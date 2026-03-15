bd80b4ad81575d51d291aae0c55884d44396b9fa
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-03-15 17:36:36 +1100

    feat: Adds Astro content collections config

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from "astro:content";
+
+const about = defineCollection({});
+const checkit = defineCollection({});
+const horus = defineCollection({});
+const sprout = defineCollection({});
+const vvstudios = defineCollection({});
+
+export const collections = {
+	about,
+	checkit,
+	horus,
+	sprout,
+	vvstudios,
+};
